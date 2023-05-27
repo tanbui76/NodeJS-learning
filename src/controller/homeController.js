c899b6b29ece6d30063177a010d132e8cf7c9544
Bùi Nguyễn Nhật Tân <84611805+tanbui76@users.noreply.github.com>
2023-05-27 17:55:46 +0700

    CRUD - complete crud user with no alert

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -4,32 +4,44 @@ let getHomepage = async (req, res) => {
     // [rows, fields] là dạng destructuring 
     //destructuring là cú pháp cho phép lấy dữ liệu từ trong array và object ra một cách nhanh chóng và dễ dàng hơn
     const [rows, fields] = await (await connection).execute('SELECT * FROM `user`'); // lấy dữ liệu từ database
-    console.log(rows);
     return res.render("index.ejs", { dataUser: rows });// trả về file index.ejs và truyền vào dataUser
 
 }
 
 let getDetailUserPage = async (req, res) => {
+
+    if (req.params.userId == null || req.params.userId == undefined || req.params.userId == "") {
+        return res.render("detail.ejs", { user: null });
+    }
     const [rows, fields] = await (await connection).execute('SELECT * FROM `user` WHERE `id` = ?', [req.params.userId]);
     return res.render("detail.ejs", { user: rows[0] });
 }
 
 let getAboutPage = (req, res) => {
-    //logic code here
     return res.send("About page");
 }
 
 let updateUserData = async (req, res) => {
-    console.log("reqbody:" + req.body.first_name);
     await (await connection).execute('UPDATE `user` SET `first_name` = ? , `last_name` = ? , `email` = ? , `address` = ? WHERE `id` = ?', [req.body.first_name, req.body.last_name, req.body.email, req.body.address, req.body.id]);
     var redirect = "/detail/" + req.body.id;
     return res.redirect(redirect);
+}
+
+let createNewUser = async (req, res) => {
+    await (await connection).execute('INSERT INTO `user` (`first_name`, `last_name`, `email`, `address`) VALUES (?, ?, ?, ?)', [req.body.first_name, req.body.last_name, req.body.email, req.body.address]);
+    return res.redirect("/");
+}
+
+let deleteUser = async (req, res) => {
+    await (await connection).execute('DELETE FROM `user` WHERE `id` = ?', [req.body.userId]);
+    return res.redirect("/");
 }
 
 module.exports = {
     getHomepage: getHomepage,
     getAboutPage: getAboutPage,
     getDetailUserPage: getDetailUserPage,
-    updateUserData: updateUserData
-
+    updateUserData: updateUserData,
+    createNewUser: createNewUser,
+    deleteUser: deleteUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -6,8 +6,11 @@ let router = express.Router();
 const initWebRoutes = (app) => {
     router.get('/', homeController.getHomepage);
     router.get('/detail/:userId', homeController.getDetailUserPage);
+    router.get('/detail', homeController.getDetailUserPage);
     router.get('/about', homeController.getAboutPage);
     router.post('/update-user', homeController.updateUserData);
+    router.post('/create-user', homeController.createNewUser);
+    router.post('/delete-user', homeController.deleteUser);
 
     return app.use("/", router);
 }
